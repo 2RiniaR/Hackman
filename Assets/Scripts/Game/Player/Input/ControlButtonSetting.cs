aa522a7c3e2e388a860050acfd782b25689f0c62
watano1168 <watano1168@gmail.com>
2020-10-01 01:30:17 +0900

    [Add] 入力ボタンの設定を構造体に移動

---
+++ Assets/Scripts/Game/Player/Input/ControlButtonSetting.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System;
+
+namespace Hackman.Game.Player {
+    [Serializable]
+    public struct ControlButtonSetting {
+        public string MoveUpButtonName;
+        public string MoveDownButtonName;
+        public string MoveLeftButtonName;
+        public string MoveRightButtonName;
+    }
+}
--- Assets/Scripts/Game/Player/Player.cs
+++ Assets/Scripts/Game/Player/Player.cs
@@ -4,10 +4,7 @@ namespace Hackman.Game.Player {
     public class Player : MonoBehaviour {
 
         [Header("操作")]
-        public string moveUpButtonName;
-        public string moveDownButtonName;
-        public string moveLeftButtonName;
-        public string moveRightButtonName;
+        public ControlButtonSetting controlButtonSetting;
 
         [Header("移動")]
         public float moveSpeed;
