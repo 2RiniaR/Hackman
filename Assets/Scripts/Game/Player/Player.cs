b089a1b2acdf8bbb44c6286c807f01b4b50a16a3
watano1168 <watano1168@gmail.com>
2020-10-01 02:23:28 +0900

    [Add] 各クラスをPlayerコンポーネントに適用

--- Assets/Scripts/Game/Player/Player.cs
+++ Assets/Scripts/Game/Player/Player.cs
@@ -12,5 +12,24 @@ namespace Hackman.Game.Player {
         [SerializeField]
         private Rigidbody2D _rigidbody;
 
+        private IInputControl inputControl;
+        private MoveUpdater moveUpdater;
+        private MoveSimulator moveSimulator;
+        private MoveStatus moveStatus;
+        private MoveSpeedStore moveSpeedStore;
+
+        private void Awake() {
+            moveStatus = new MoveStatus();
+            moveSimulator = new MoveSimulator(_rigidbody);
+            moveSpeedStore = new MoveSpeedStore(moveSpeed);
+            moveUpdater = new MoveUpdater(moveSimulator, moveStatus);
+            inputControl = new ButtonInputControl(controlButtonSetting);
+        }
+
+        private void OnDestroy() {
+            moveUpdater.Dispose();
+            inputControl.Dispose();
+        }
+
     }
 }
