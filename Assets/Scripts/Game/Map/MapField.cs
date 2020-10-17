7f20f21c4fc7c105c893ad02a40f8c5ff70e93f3
watano1168 <watano1168@gmail.com>
2020-10-18 01:46:50 +0900

    [Add] Mapの要素とMap全体のクラスを追加

---
+++ Assets/Scripts/Game/Map/MapField.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System;
+using UniRx;
+
+namespace Hackman.Game.Map {
+
+    public struct UpdateFieldElementEventArgs {
+        public readonly MapField TargetField;
+        public readonly Vector2Int UpdatedPosition;
+        public readonly MapElement? ElementAfterUpdate;
+
+        public UpdateFieldElementEventArgs(MapField target, Vector2Int pos) {
+            TargetField = target;
+            UpdatedPosition = pos;
+            ElementAfterUpdate = target.GetElement(pos);
+        }
+    }
+
+    public class MapField {
+
+        // 座標系はワールド座標と同じ
+        // x: 右方向が正, 左方向が負
+        // y: 上方向が正, 下方向が負
+        private readonly MapElement[,] elements;
+        public readonly int Width;
+        public readonly int Height;
+
+        private readonly Subject<UpdateFieldElementEventArgs> onFieldElementUpdated = new Subject<UpdateFieldElementEventArgs>();
+        public IObservable<UpdateFieldElementEventArgs> OnFieldElementUpdated => onFieldElementUpdated;
+
+        public MapField(MapElement[,] elements) {
+            this.elements = elements;
+            Width = elements.GetLength(0);
+            Height = elements.GetLength(1);
+        }
+
+        public void UpdateElement(int x, int y, MapElement element) {
+            UpdateElement(new Vector2Int(x, y), element);
+        }
+
+        public void UpdateElement(Vector2Int pos, MapElement element) {
+            if (!IsFieldRange(pos)) {
+                return;
+            }
+            elements[pos.x, pos.y] = element;
+            onFieldElementUpdated.OnNext(
+                new UpdateFieldElementEventArgs(this, pos)
+            );
+        }
+
+        public MapElement[,] GetAllElements() {
+            return (MapElement[,])elements.Clone();
+        }
+
+        public MapElement? GetElement(int x, int y) {
+            return GetElement(new Vector2Int(x, y));
+        }
+
+        public MapElement? GetElement(Vector2Int pos) {
+            if (!IsFieldRange(pos)) {
+                return null;
+            }
+            return elements[pos.x, pos.y];
+        }
+
+        public bool IsFieldRange(Vector2Int pos) {
+            return pos.x.IsRange(0, Width - 1) && pos.y.IsRange(0, Height - 1);
+        }
+
+        public bool IsFieldRange(Vector2 pos) {
+            return Mathf.FloorToInt(pos.x).IsRange(0, Width - 1) && Mathf.FloorToInt(pos.y).IsRange(0, Height - 1);
+        }
+
+    }
+
+}
